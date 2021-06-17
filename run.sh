9518c9e25d1a28a9f7dc0f635b2ce883001f5ed3
Byeongho Kang <byeongho.kang@yahoo.com>
2021-06-17 01:23:56 +0000

    added docker build & run scripts

---
+++ run.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+su -s /bin/bash -c "/home/elasticsearch/elasticsearch-7.13.2/bin/elasticsearch -d" -g elasticsearch elasticsearch
+sleep 5
+nohup /opt/conda/bin/python /root/tokenize-api/tokenize_api_server.py > /tmp/tokenize_api_server.log &
