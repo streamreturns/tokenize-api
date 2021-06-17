aaf1aebf7464afd4ad85187c742369a626e9a0e9
Byeongho Kang <byeongho.kang@yahoo.com>
2021-06-17 04:23:52 +0000

    fixed run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 su -s /bin/bash -c "/home/elasticsearch/elasticsearch-7.13.2/bin/elasticsearch -d" -g elasticsearch elasticsearch
 sleep 5
-/opt/conda/bin/python /root/tokenize-api/tokenize_api_server.py > /tmp/tokenize_api_server.log 
+nohup /opt/conda/bin/python /root/tokenize-api/tokenize_api_server.py > /tmp/tokenize_api_server.log &
+tail -f /tmp/tokenize_api_server.log
